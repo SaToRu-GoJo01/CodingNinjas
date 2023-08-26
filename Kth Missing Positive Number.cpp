5440b64c915bde60b8f02a5ffa8c36de06995027
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-08-26 19:40:36 +0530

    Create Kth Missing Positive Number.cpp

---
+++ Kth Missing Positive Number.cpp
@@ -0,0 +1,17 @@
+#include <unordered_map>
+int missingK(vector<int> vec, int n, int k) {
+    if(vec[0]>k) return k;
+    int low = 0,high = n - 1;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        // cout << " - " << vec[mid] << endl;
+        if((vec[mid]- mid - 1)>k){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    // cout << vec[high] << endl;
+    return (vec[high] + (k-(vec[high]-high-1)));
+}
