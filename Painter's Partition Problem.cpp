c57c07198455aee7900213a82339b90895f4122e
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-09-02 17:17:32 +0530

    Create Painter's Partition Problem.cpp

---
+++ Painter's Partition Problem.cpp
@@ -0,0 +1,35 @@
+#include <limits.h>
+int func(vector<int> a,int mid){
+    int sum = 0,partitions=1;
+    for(int i=0;i<a.size();i++){
+        if(a[i]+sum <= mid){
+            sum += a[i];
+        }
+        else{
+            partitions++;
+            sum = a[i];
+        }
+    }
+    return partitions;
+}
+int largestSubarraySumMinimized(vector<int> a, int k) {
+    int low = INT_MIN,high=0;
+    for(auto i:a){
+        high += i;
+        low = max(low,i);
+    }
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(k >= func(a,mid)){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    return largestSubarraySumMinimized(boards,k);
+}
