9ec7d81837110ed9f10301911760751c3313d4ed
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-10-20 05:35:44 +0530

    Create Search in a Linked List.cpp

---
+++ Search in a Linked List.cpp
@@ -0,0 +1,37 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+    template <typename T>
+    class Node
+    {
+    public:
+        T data;
+        Node<T> *next;
+        Node()
+        {
+            this->data = 0;
+            this->next = NULL;
+        }
+        Node(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+        Node(T data, T* next)
+        {
+            this->data = data;
+            this->next = next;
+        }
+    };
+
+*****************************************************************/
+
+int searchInLinkedList(Node<int> *head, int k) {
+    while(head != NULL){
+        if(head->data == k) 
+        return 1;
+        head = head->next;
+    }
+    return 0;
+}
